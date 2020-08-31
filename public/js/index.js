5a0cddfecb28bbd38bf2b4185b1b9df95b2535e3
diegotorre5 <diegotorre5@github.com>
2020-08-31 21:24:29 +0200

    Location button

--- public/js/index.js
+++ public/js/index.js
@@ -13,12 +13,38 @@ socket.on('newMessage', function(message){
   jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(message){
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a>My current location</a>');
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function(e){
   e.preventDefault();
+  console.log('CLick');
   socket.emit('createMessage', {
     from: 'User',
     text: jQuery('[name=message]').val()
   }, function(){
     jQuery('[name=message]').val('')
+  });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function() {
+  if(!navigator.geolocation){
+    return alert('Geolocation is not supported by your browser');
+  }
+  navigator.geolocation.getCurrentPosition(function (position) {
+    console.log(position);
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    }, function(){
+      alert('Unable to fetch location.')
+    });
   });
 });
--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 var app = express();
 var server = http.createServer(app);
@@ -34,4 +34,9 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('newMessage',generateMessage (message.from, message.text));
     callback('Server callback');
   });
+
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  });
+
 });
